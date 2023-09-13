5b134edec77c7b618118cea5fe44dc4f375f275b
Avikshit Banerjee <avikshitbanerjee@gmail.com>
2023-09-13 22:43:10 +0100

    deployment

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
 import pickle
+import numpy as np
 
 popular_df = pickle.load(open('popular.pkl','rb'))
 pt = pickle.load(open('pt.pkl','rb'))
@@ -22,6 +22,25 @@ def index():
 def recommend_ui():
     return render_template('recommend.html')
 
+@app.route('/recommend_books',methods=['post'])
+def recommend():
+    user_input = request.form.get('user_input')
+    index = np.where(pt.index == user_input)[0][0]
+    similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:5]
+
+    data = []
+    for i in similar_items:
+        item = []
+        temp_df = books[books['Book-Title'] == pt.index[i[0]]]
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
+
+        data.append(item)
+
+    print(data)
+
+    return render_template('recommend.html',data=data)
 
 if __name__ == "__main__":
     app.run(debug=True)
